442de2dbbe88172fd83f05d64c0f92179419ee17
Carlos <calososla@gmail.com>
2024-11-03 10:27:18 -0300

    Melhorias na responsividade

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll", function() {
-    const elements = document.querySelectorAll(".sobre-container, .artista-container, .album-container");
-    elements.forEach(element => {
-        const position = element.getBoundingClientRect().top;
-        if (position < window.innerHeight) {
-            element.classList.add("in-view");
-        }
-    });
-});
